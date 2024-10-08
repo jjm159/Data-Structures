8276ffc06dc66fb5d107ec2f67daeafc462d4dde
jjm159 <wjdwoaud159@gmail.com>
2024-10-08 04:05:04 +0000

    Binary_Search_Tree/Q3_F_BST.c

--- Binary_Search_Tree/Q3_F_BST.c
+++ Binary_Search_Tree/Q3_F_BST.c
@@ -91,7 +91,27 @@ int main()
 
 void preOrderIterative(BSTNode *root)
 {
-	 /* add your code here */
+
+	Stack s; 
+	s.top = NULL;
+
+	BSTNode* current = NULL;
+	push(&s, root);
+
+	while (!isEmpty(&s))
+	{
+		current = pop(&s);
+
+		printf("%d ", current->item);
+		
+		if (current->right != NULL) {
+			push(&s, current->right);
+		}
+		if (current->left != NULL) {
+			push(&s, current->left);
+		}
+	}
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////
